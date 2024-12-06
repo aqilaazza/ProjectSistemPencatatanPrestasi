87791bd8299a74072852459e46a15e25a34361c0
aqilaazza <aqilanurazza123@gmail.com>
2024-12-06 11:58:55 +0700

    update

--- projectAkhirV2/mahasiswa/up_nonakademik.php
+++ projectAkhirV2/mahasiswa/up_nonakademik.php
@@ -130,8 +130,19 @@
     <div class="container">
         <h2>Data Prestasi Non-Akademik</h2>
         <form action="#">
+            <label for="nama-kompetisi">Nama Kompetisi</label>
             <input type="text" placeholder="Nama Kompetisi" required />
-            
+            <label for="ID Dosen Pembimbing">ID Dosen Pembimbing</label>
+            <input type="text" placeholder="ID Dosen Pembimbing" required />
+            <label for="nim-mahasiswa">NIM Mahasiswa</label>
+            <input type="text" placeholder="NIM Mahasiswa" required />
+            <label for="peran">Peran dalam Kompetisi:</label>
+            <select id="peran" name="peran" required>
+                <option value="" disabled selected>Pilih Peran</option>
+                <option value="ketua">Ketua</option>
+                <option value="Anggota">Anggota</option>
+                <option value="Personal">Personal</option>
+            </select>
             <!-- Jenis Kompetisi: Dropdown dengan placeholder -->
             <label for="jenis-kompetisi">Jenis Kompetisi:</label>
             <select id="jenis-kompetisi" name="jenis-kompetisi" required>
@@ -157,7 +168,7 @@
                 <option value="">Internasional</option>
                 <option value="">Lainnya</option>
             </select>
-
+            <label for="Tanggal-Penyelenggaraan">Tanggal Penyelenggaraan</label>
             <input type="date" placeholder="Tanggal Penyelenggaraan" required />
             <label for="upload-dokumentasi">Upload Dokumentasi</label>
             <input type="file" id="upload-dokumentasi" required />
@@ -167,13 +178,24 @@
             <input type="file" id="upload-karya" required />
             <label for="upload-surat-tugas">Upload Surat Tugas</label>
             <input type="file" id="upload-surat-tugas" required />
+            <label for="penyelenggara">Penyelenggara</label>
             <input type="text" placeholder="Penyelenggara" required />
-            <input type="text" placeholder="Peringkat" required />
+            <label for="peringkat">Peringkat:</label>
+            <select id="peringkat" name="Peringkat" required>
+                <option value="" disabled selected>Pilih Peringkat</option>
+                <option value="juara1">Juara 1</option>
+                <option value="juara2">Juara 2</option>
+                <option value="juara3">Juara 3</option>
+                <option value="harapan1">Harapan 1</option>
+                <option value="harapan2">Harapan 2</option>
+                <option value="harapan3">Harapan 3</option>
+                <option value="lainnya">Lainnya</option>
+            </select>
 
             <button type="submit">Simpan</button>
         </form>
         <div class="login-link">
-            <p><a href="dashboard_mahasiswa.php">Kembali ke Dashboard</a></p>
+            <p><a href="dashboard_mahasiswa.php">Batal</a></p>
         </div>
     </div>
 </body>
