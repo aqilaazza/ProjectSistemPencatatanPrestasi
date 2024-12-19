26557d6f2b5210e1f8b7eb272afbefb6aeb26996
REZA ANGELINA FEBRIYANTI_SIB1C <145529391+RezaAngelinaFebriyanti@users.noreply.github.com>
2024-12-19 13:17:31 +0700

    input ip mhs admin

--- projectAkhirV2/dashboard/dashboardAdmin.php
+++ projectAkhirV2/dashboard/dashboardAdmin.php
@@ -16,7 +16,7 @@
             <li><a href="../admin/biodata_admin.php">Biodata Admin</a></li>
             <li><a href="../admin/biodata_dosen.php">Biodata Dosen</a></li>
             <li><a href="../admin/biodata_mahasiswa.php">Biodata Mahasiswa</a></li>
-            <li><a href="../admin/prestasi_akademik.php">Unggah Prestasi Akademik</a></li>
+            <li><a href="../admin/ipMhs.php">Unggah IP Mahasiswa</a></li>
             <li><a href="../admin/validasi_prestasi.php">Validasi Prestasi Non-Akademik</a></li>
             <li><a href="#" onclick="confirmLogout()">Keluar</a></li>
         </ul>
